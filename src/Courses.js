a5fa673c25096628402d1a026eeb40fc71e2901d
satayyeb <salitayyeb@gmail.com>
2024-02-04 15:15:56 +0330

    Fix enroll button

--- src/Courses.js
+++ src/Courses.js
@@ -10,14 +10,13 @@ import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Dialog } from "@mui/material";
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import {Dialog} from "@mui/material";
 import MainFooter from "./MainFooter";
-import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
 import CoursePage from "./CoursePage";
-
-
+import axios from "axios";
 
 
 // const MainFooter = React.lazy(() => import('./MainFooter.js'));
@@ -26,22 +25,22 @@ import CoursePage from "./CoursePage";
 const defaultTheme = createTheme();
 
 export default function Courses() {
-  const [cards, setCards] = useState([]);
-  const [openedCoursePageId, setOpenedCoursePageId] = useState(null);
+    const [cards, setCards] = useState([]);
+    const [openedCoursePageId, setOpenedCoursePageId] = useState(null);
 
-  useEffect(() => {
-    (async () => {
-      const data = await fetch(
-        `${process.env.REACT_APP_BACKEND_URL}/courses/`
-      ).then((res) => res.json());
-      setCards(data);
-    })();
-  }, []);
+    useEffect(() => {
+        (async () => {
+            const data = await fetch(
+                `${process.env.REACT_APP_BACKEND_URL}/courses/`
+            ).then((res) => res.json());
+            setCards(data);
+        })();
+    }, []);
 
     return (
         <>
             <ThemeProvider theme={defaultTheme}>
-                <CssBaseline />
+                <CssBaseline/>
                 <main>
                     {/* Hero unit */}
                     <Box
@@ -66,7 +65,7 @@ export default function Courses() {
                                 interested in and read the description of its.
                             </Typography>
                             <Stack
-                                sx={{ pt: 2 }}
+                                sx={{pt: 2}}
                                 direction="row"
                                 spacing={2}
                                 justifyContent="center"
@@ -75,13 +74,13 @@ export default function Courses() {
                             </Stack>
                         </Container>
                     </Box>
-                    <Container sx={{ py: 0 }} maxWidth="md">
+                    <Container sx={{py: 0}} maxWidth="md">
                         {/* End hero unit */}
                         <Grid container spacing={4}>
                             {cards.map((card) => (
                                 <Grid item key={card} xs={12} sm={6} md={4}>
                                     <Card
-                                        sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                                        sx={{height: '100%', display: 'flex', flexDirection: 'column'}}
                                     >
                                         <CardMedia
                                             component="div"
@@ -91,7 +90,7 @@ export default function Courses() {
                                             }}
                                             image={`https://picsum.photos/seed/foobar${card.name}/200`}
                                         />
-                                        <CardContent sx={{ flexGrow: 1 }}>
+                                        <CardContent sx={{flexGrow: 1}}>
                                             <Typography gutterBottom variant="h5" component="h2">
                                                 {/*{card.name}*/}
                                                 {card.name}
@@ -100,14 +99,28 @@ export default function Courses() {
                                                 {card.description}
                                             </Typography>
                                             <br/>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button size="medium" onClick={() => {
+                                                try {
+                                                    axios.defaults.withCredentials = true;
+                                                    axios.post(`/courses/${card.pk}/enroll`)
+                                                        .then(() => {
+                                                            window.location.assign('/Dashboard')
+                                                        })
+                                                        .catch((response) => {
+                                                            console.log(response)
+                                                            alert(response.response.data)
+                                                        });
+                                                } catch (error) {
+                                                    alert('Signup failed. Please try again.');
+                                                }
+                                            }}>
+                                                Enroll
+                                            </Button>
                                             <Typography>
                                                 price: {card.price}
                                             </Typography>
-                                        </CardContent>
-                                        <CardActions>
-                                            <Button size="medium"   onClick={()=>setOpenedCoursePageId(card.pk)}>
-                                                Details & Enroll
-                                            </Button>
                                         </CardActions>
                                     </Card>
                                 </Grid>
@@ -117,9 +130,9 @@ export default function Courses() {
                 </main>
             </ThemeProvider>
             <Dialog open={openedCoursePageId} onClose={() => setOpenedCoursePageId(null)}>
-                <CoursePage courseId={openedCoursePageId} />
+                <CoursePage courseId={openedCoursePageId}/>
             </Dialog>
-            <MainFooter />
+            <MainFooter/>
         </>
     );
 }
